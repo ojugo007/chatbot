36967576d1a06449d48a080891eb107d7036cd2c
silas oj <silasojugo@gmail.com>
2025-04-19 10:14:26 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const {createServer} = require("node:http")
+const {Server} = require("socket.io")
+const join = require("path").join
+require("dotenv").config()
+
+const PORT = process.env.PORT || 8080
+
+const app = express()
+const server = createServer(app)
+const io = new Server(server)
+app.use(express.static("public"))
+
+io.on("connection", (socket)=>{
+    console.log( "a user connected " + socket.id)
+    console.log(socket.listenerCount)
+    // socket.on("greeting", (data)=>{
+    //     console.log(data)
+    // })
+    socket.emit("greeting", "Welcome to Iya-Bashira Buka! My name na Padi, your assistant bot.")
+
+    const options = [
+        'Select 1 to Place an order',
+        'Select 99 to checkout order',
+        'Select 98 to see order history',
+        'Select 97 to see current order',
+        'Select 0 to cancel order'
+    ]
+    socket.emit("menus", options )
+
+    socket.on("selected", (message)=>{
+        console.log(message)
+        switch(message){
+            case '1':
+                socket.emit( "request" ,`you selected ${message} to make an order`)
+                // redirect to order page or show a list of food to order from
+                break;
+            case '99':
+                socket.emit( "request" ,`you selected ${message} to checkout order`)
+                // redirect to checkout page
+                break;
+            case '98':
+                
+                socket.emit("request",`you selected ${message} to see order history`)
+                // fetch order history of this user
+                break;
+            case '97':
+                socket.emit("request",`you selected ${message} to see current order`)
+                // show the current order by checking the order id and comparing
+                break;
+            case '0':
+                socket.emit("request",`you selected ${message} to cancel an order`)
+                // cancel the current order, more like delete
+                break;
+            default:
+                socket.emit("request","unkown selection")
+                break;
+        }
+    })
+
+    io.on("disconnect", (socket)=>{
+        console.log(socket.id, " has disconnected")
+    })
+})
+
+app.get("/", (req, res)=>{
+    res.sendFile(join(__dirname , "index.html"))
+})
+
+server.listen(PORT, ()=>{
+    console.log(`server is running on http://localhost:${PORT}`)
+})
+
+
---
+++ public/js/main.js
@@ -0,0 +1,78 @@
+
+const greeting = document.querySelector("#bot-welcome-message");
+const option_container = document.querySelector(".option-container");
+const option_grid = document.querySelector("#option-grid");
+const list = document.querySelector("#list");
+const user_selection = document.querySelector("#user-message");
+const send_message = document.querySelector("#send-message");
+const message_box = document.querySelector("#message-box");
+
+
+
+const socket = io("http://localhost:8080")
+
+socket.on("greeting", (data)=>{
+    console.log(data)
+    setTimeout(()=>{
+        greeting.innerHTML = data
+    }, 1000);
+})
+
+socket.on("menus", (options)=>{
+    option_container.classList.add("hidden")
+    function getList(){
+        for(option of options){
+            option_container.classList.remove("hidden")
+            const list = document.createElement('span')
+            const listText = document.createTextNode(option)
+            list.appendChild(listText)
+            option_grid.appendChild(list)
+        }
+
+    }
+
+    setTimeout(()=>{
+        getList()
+    }, 2000);
+})
+
+
+
+
+send_message.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    const message = list.value
+    socket.emit("selected", message)
+    const user_container = document.createElement('div');
+    user_container.classList.add("selected-options");
+    const user_message_el = document.createElement('p');
+    user_message_el.classList.add("user-message")
+    const user_profile_image = document.createElement("img")
+    user_profile_image.width = 30;
+    user_profile_image.height = 30;
+    user_profile_image.alt = "user profile image";
+    user_profile_image.src="/images/user.png";
+    const user_text = document.createTextNode(message)
+    user_message_el.appendChild(user_text)
+    user_container.appendChild(user_message_el)
+    user_container.appendChild(user_profile_image)
+    message_box.appendChild(user_container)
+    list.value = ""
+} )
+
+socket.on("request", (message)=>{
+    const bot_reponse_container = document.createElement('div');
+    bot_reponse_container.classList.add("welcome-message-container");
+    const bot_message_el = document.createElement('p');
+    bot_message_el.classList.add("bot-response")
+    const bot_image = document.createElement("img")
+    bot_image.width = 30;
+    bot_image.height = 30;
+    bot_image.alt = "bot image";
+    bot_image.src="/images/bot.png";
+
+    bot_message_el.appendChild(document.createTextNode(message))
+    bot_reponse_container.appendChild(bot_image)
+    bot_reponse_container.appendChild(bot_message_el)
+    message_box.appendChild(bot_reponse_container)
+})
